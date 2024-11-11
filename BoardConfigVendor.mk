3577a320e4a121d594de9bb4edecc1a443ecb0de
josexda <josegustavo6@protonmail.com>
2024-11-11 17:32:53 -0300

    revert "hawao: Import firmware from T2SE33.73-23-2"
    This reverts commit 238671196f599f5c4624256ec494d98a91782614.
    
    Change-Id: I00d1c445e064a191fa37e612bf92ff76bc6e1b42

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,22 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    abl \
-    bluetooth \
-    devcfg \
-    dsp \
-    fsg \
-    hyp \
-    keymaster \
-    logo \
-    modem \
-    prov \
-    qupfw \
-    rpm \
-    storsec \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
